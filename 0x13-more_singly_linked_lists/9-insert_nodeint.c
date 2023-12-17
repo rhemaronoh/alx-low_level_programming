56ade4be87dc62aab412cb193532e6cbcd5c20d0
rhemaronoh <rhema1099@gmail.com>
2023-12-17 13:01:10 -0800

    ./j

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * insert_nodeint_at_index - inserts a new node in a listint_t list
  * at a given position
- * @head pointer to the first node in the listint_t list
+ * @head: pointer to the first node in the listint_t list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
  *
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
-	listint_t *temp;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
